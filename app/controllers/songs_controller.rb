8b89db28d139533db4faeedde1e26da4f4b4651c
Vic Lam <lam.vic.k@gmail.com>
2017-01-02 23:01:46 -0500

    Senor Grumps

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,33 @@
+class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update]
+
+  # Index action
+  def index
+    @songs = Song.all
+  end
+
+  # New actions
+  def new
+    @song = Song.new
+  end
+  def create
+    @song = Song.create(song_params)
+    redirect_to song_path(@song)
+  end
+
+  # Edit actions
+  def update
+    @song.update(song_params)
+    redirect_to song_path(@song)
+  end
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end
+end
